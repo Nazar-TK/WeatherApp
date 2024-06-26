29ba5dae82265548fdb0b3ac7951cf96f82128c7
Nazar Kalapun <ntkalapun@gmail.com>
2024-06-26 12:43:22 +0300

    Added DI to project.

---
+++ app/src/main/java/com/example/weatherapp/di/AppModule.kt
@@ -0,0 +1,50 @@
+package com.example.weatherapp.di
+
+import android.app.Application
+import com.example.weatherapp.data.location.LocationTrackerImpl
+import com.example.weatherapp.data.remote.WeatherApi
+import com.example.weatherapp.data.repository.WeatherRepositoryImpl
+import com.example.weatherapp.domain.location.LocationTracker
+import com.example.weatherapp.domain.repository.WeatherRepository
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideWeatherApi(): WeatherApi {
+        return Retrofit.Builder()
+            .baseUrl(WeatherApi.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(WeatherApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideWeatherRepository(api: WeatherApi): WeatherRepository {
+        return WeatherRepositoryImpl(api)
+    }
+
+    @Provides
+    @Singleton
+    fun provideFusedLocationProviderClient(app: Application): FusedLocationProviderClient {
+        return LocationServices.getFusedLocationProviderClient(app)
+    }
+
+    @Provides
+    @Singleton
+    fun provideLocationTracker(locationClient: FusedLocationProviderClient, app: Application): LocationTracker {
+        return LocationTrackerImpl(locationClient, app)
+    }
+}
